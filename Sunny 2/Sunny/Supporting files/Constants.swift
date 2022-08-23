5617ccef3728fbb45405c8710bc7f53e64db8f63
VMGit <91979333+SelfishMack@users.noreply.github.com>
2022-08-23 18:27:46 +0300

    Add files via upload

---
+++ Sunny 2/Sunny/Supporting files/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Sunny
+//
+
+
+import Foundation
+
+let apiKey = "358389cc2e7b7f987ac85f1075b911c6"
---
+++ Sunny 2/Sunny/View Controllers/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Sunny
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    
+    var networkWeatherManager = NetworkWeatherManager()
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
+            self.networkWeatherManager.fetchCurrentWeather(forCity: city)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        networkWeatherManager.onCompletion = { currentWeather in
+            print(currentWeather.cityName)
+        }
+        networkWeatherManager.fetchCurrentWeather(forCity: "London")
+    }
+}
+
+
